d74ca2c2a414cac28e12a6f771e4af4ad318851a
Abdullah Ibrahimagaoglu <aagaoglupc@gmail.com>
2024-06-09 13:47:24 +0300

    Create circleAreaSurroundings.java

---
+++ src/circleAreaSurroundings.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class circleAreaSurroundings {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		final float pi = 3.141592653589793238462643383279502884197f;
+		
+		System.out.println("Enter your flat's radius : ");
+		int radius = scanner.nextInt();
+		
+		float circleArea = pi * radius * radius;
+		float circleSurrounding =  pi * 2 * radius;
+		
+		System.out.println("Your circle  area is : " + circleArea);
+		System.out.println("Your circle  surroundings is : " + circleSurrounding);
+
+	}
+
+}
